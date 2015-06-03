b30faa7e01d3bbcf04849a889182ac5b7e54e9f6
Blazers <308802880@qq.com>
2015-06-03 23:20:09 +0800

    Remove delete files in the Master Branch
    - Add contact list - chat tab combine control
    
    TODO:
    
       1: Add click tab callback
       2: Add div return effect
       3: Add form page

--- public/javascripts/chat/contact.js
+++ public/javascripts/chat/contact.js
@@ -8,18 +8,25 @@ $(function(){
         // 02  Get target ul element  --- div
         // 03  Compare if exist  if   bring it front   else  add to it  
         ifExistBringItFront(contactId);
-        // 04  Call replace div function
+        // 04  Call replace div function if add then adjust the div scrollbar
         return false;
     })
 
     function ifExistBringItFront(contactId) {
+        div.children().removeClass("active")
+        var tag = false;
         div.children("li").each(function(index) {
-            if ($(this).attr("role") == contactId) {
-                return true;
+            if ($(this).attr("conversation") == contactId) {
+                tag = true;
+                $(this).addClass("active").attr("conversation", contactId).insertBefore(div.children().first());
+                return false;
             }
         });
         // Add and return false
-        var add = '<li role="presentation"><a href="#">ABC </a></li>'
-        return false;
+        if (!tag) {
+            var add = '<li role="presentation" class="active" conversation="'+contactId+'"><a href="#">'+contactId+'</a></li>';
+            div.children("li").first().before(add);
+        }
+        return tag;
     }
 });
