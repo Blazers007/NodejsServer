6664f3c8b777b39eed9df01667967c20d9ad3b94
Blazers <blazers_d_ar@outlook.com>
2015-06-12 17:14:07 +0800

    Use bootswatch and add little angular module in the version

--- public/javascripts/appointment/app.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by liang on 2015/6/4.
- */
---
+++ public/javascripts/appointment/controller/appoint.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liang on 2015/6/12.
+ */
+
+/* 传建一个controller 为相应的数据填充内容 */
+appModule.controller('appointment-controller', [
+    '$scope', function($scope){
+        $scope.status = '申请中'
+    }
+]);
